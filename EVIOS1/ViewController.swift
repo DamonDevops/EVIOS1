45689b685cc7642d34784ed8c379b522ca054eb9
Damon Jacquemin <damon.jacquemin@gmail.com>
2023-06-30 08:45:19 +0200

    initial commit

---
+++ EVIOS1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EVIOS1
+//
+//  Created by Student04 on 30/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ EVIOS1UITests/EVIOS1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  EVIOS1UITestsLaunchTests.swift
+//  EVIOS1UITests
+//
+//  Created by Student04 on 30/06/2023.
+//
+
+import XCTest
+
+final class EVIOS1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
